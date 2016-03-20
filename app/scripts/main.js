5c92edc1671545e40ac3fed48a397523973a4fd6
nelson <nelson119@outlook.com>
2016-03-21 07:38:52 +0800

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,50 @@
+'use strict';
+/*eslint-disable new-cap, no-unused-vars, 
+  no-use-before-define, no-trailing-spaces, 
+  no-mixed-spaces-and-tabs, no-multi-spaces, camelcase, no-loop-func,
+  comma-spacing,no-spaced-func,space-infix-ops,
+  key-spacing */
+/*global  $, TweenMax, Elastic*/
+
+
+
+$(function(){
+
+	var stages = ['index', 'legal', 'wheel', 'fill'];
+
+	$.each(stages, function(i, s){
+		$('.btn-' + s).on('click', function(){
+			gotoStage(s);
+		});
+	});
+
+	function gotoStage(stage){
+		$.each(stages, function(idx, rm){
+			if(this !== stage){
+				$('body').removeClass(rm);
+			}
+		});
+		$('body').addClass(stage);
+	}
+
+	$('.wheel-content .go').one('click', function(){
+		var pick = $('.wheel-content .wheel').attr('data-pick') * 1 - 1;
+		var count = $('.wheel-content .wheel').attr('data-item-count') * 1;
+		var direction = 1;
+
+		if(count === 6 || count === 7 || count === 8){
+			direction = -1;
+		}
+
+		var rotate = (360 / count * pick * direction);
+		rotate += 360 * Math.round(Math.random() * 15 + 5) * direction;
+		TweenMax.to('.wheel-content .wheel figure .wheel-background, .wheel figure ul', 25,{
+		  rotationZ: rotate,
+		  ease: Elastic. easeOut.config( 1, 0.8),
+		  onComplete: function(){
+		  	$('.btn-fill').removeClass('hide').addClass('in');
+		  }
+		});
+
+	});
+});
